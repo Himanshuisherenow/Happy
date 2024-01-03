7e6e1e5a8a17e7ddd53d32256198ea90b3e9ecf0
Chavda Himanshu H <himanshuisherenow@gmail.com>
2024-01-04 00:48:17 +0530

    context api and its provider

---
+++ contextApi/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+import UserContext from './UserContext'
+import UserContextProvider from './UserContextProvider'
+ 
+function App() {
+    
+
+  return (
+   <UserContextProvider>
+
+    </UserContextProvider>
+  )
+}
+
+export default App
---
+++ contextApi/src/UserContextProvider.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import UserContext from "./UserContext";
+
+
+const UserContextProvider   = ({childeren})=>{
+
+    const [user, setUser] = useState(null)
+    return(
+        <UserContext.Provider value={{user, setUser}}>   
+            {childeren}
+        </UserContext.Provider>
+    )
+}
+
+export default UserContextProvider
